c63bfb5d05971f7ad8e3f014310c0bd2fb85a363
Walter Griebl <waltergriebl@gmail.com>
2026-05-03 18:41:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,14 @@
-const CACHE_NAME = "wuerfelduell-v6";
+const CACHE_NAME = "wuerfelduell-v7";
 const APP_FILES = [
   "./",
   "./index.html",
   "./styles.css",
   "./script.js",
   "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/icon-1024.png",
-  "./icons/apple-touch-icon.png",
-  "./icons/favicon-32.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png",
+  "./favicon-32.png",
 ];
 
 self.addEventListener("install", (event) => {
