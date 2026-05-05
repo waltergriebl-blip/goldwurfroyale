92dd885a0eecbd164cfff79fc241ba4fea4ccf80
Walter Griebl <walter.griebl@example.local>
2026-05-06 00:18:03 +0200

    Update Webseite

--- script.js
+++ script.js
@@ -664,6 +664,6 @@ render();
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("sw.js?v=50").then((registration) => registration.update()).catch(() => {});
+    navigator.serviceWorker.register("sw.js?v=51").then((registration) => registration.update()).catch(() => {});
   });
 }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wuerfelduell-v50";
+const CACHE_NAME = "wuerfelduell-v51";
 const APP_FILES = [
   "./",
   "./index.html",
